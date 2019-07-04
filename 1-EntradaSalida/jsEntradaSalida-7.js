f65b65f854443684d9f89c62ac8f2cc3305ee76e
6669000 <52494342+6669000@users.noreply.github.com>
2019-07-04 10:51:51 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,59 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+    var numeroUno
+    var numeroDos
+    var suma
+    numeroUno=document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    suma=numeroUno+numeroDos;
+    alert("la suma es"+suma);
+
 	
 }
 
 function restar()
 {
+    var numeroUno
+    var numeroDos
+    var restar
+    numeroUno=document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos); 
+    restar=numeroUno-numeroDos;
+    alert("la resta es"+restar);
 	
 }
 
 function multiplicar()
 { 
-	
+    var numeroUno
+    var numeroDos
+    var multiplicar
+    numeroUno=document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    multiplicar=numeroUno*numeroDos;
+    alert("la multiplicacion es"+multiplicar);
+
 }
 
 function dividir()
 {
+    var numeroUno
+    var numeroDos
+    var dividir
+    numeroUno=document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    dividir=numeroUno/numeroDos;
+    alert("la divicion es"+dividir);
+
 	
 }
 
